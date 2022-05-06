fab8fa20e46c8243a56bc0d70f1f3c5831714bdf
nikita <adrbrug@gmail.com>
2022-05-06 20:16:26 +0300

    ++

--- server/src/ru/ifmo/serverapp/ServerApplication.java
+++ server/src/ru/ifmo/serverapp/ServerApplication.java
@@ -1,23 +1,8 @@
 package ru.ifmo.serverapp;
 
-import java.io.*;
 
 public class ServerApplication {
     public static void main(String[] args) {
-
-        SomeInterface<String> someInterface = new SomeInterface<>() {
-            @Override
-            public void someVoid1(String s) {
-                // реализация метода
-            }
-
-            @Override
-            public String someVoid2() {
-                // реализация метода
-                return null;
-            }
-        };
-
         new Server(8999).start();
     }
 }
--- server/src/ru/ifmo/serverapp/SomeInterface.java
+++
@@ -1,6 +0,0 @@
-package ru.ifmo.serverapp;
-
-public interface SomeInterface<T> {
-    void someVoid1(T t);
-    T someVoid2();
-}
